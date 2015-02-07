2436ba5f0947a940d7eb8304fb4ceffe91f8a31f
Eric Lemieux <ericlemieux2@gmail.com>
2015-02-07 13:54:46 -0500

    Fixed the way debug and release libs were being linked for bullet

--- src/Game.h
+++ src/Game.h
@@ -1,21 +1,4 @@
-/*
------------------------------------------------------------------------------
-Filename:    TutorialApplication.h
------------------------------------------------------------------------------
-
-This source file is part of the
-   ___                 __    __ _ _    _ 
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
-*/
-#ifndef __TutorialApplication_h_
-#define __TutorialApplication_h_
+#pragma once
 
 #include "BaseApplication.h"
 #include "DotScene.h"
@@ -44,5 +27,3 @@ protected:
 public:
 	std::shared_ptr<Scene> mCurrentScene;
 };
-
-#endif // #ifndef __TutorialApplication_h_
