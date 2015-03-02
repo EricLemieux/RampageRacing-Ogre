ffc40ce16b39d570acb1f13b4e481527eb1c93bd
Eric Lemieux <ericlemieux2@gmail.com>
2015-03-02 18:43:05 -0500

    Hotfix for hitting the spacebar twice

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -406,7 +406,7 @@ void MenuScene::KeyPressed(const OIS::KeyEvent &arg)
 		}
 		else if (currentSubMenu == sm_LevelSelect)
 		{
-			SwapToGameplayLevel("level1");
+			SwapToGameplayLevel(mCurrentSelectedLevel);
 		}
 	}
 
